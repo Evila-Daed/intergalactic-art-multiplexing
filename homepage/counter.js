eaf66b8347a5414063d0e6c7531c9d02b7471ec9
EvilaDaed97 <maccario1997@gmail.com>
2026-08-07 22:09:20 +0200

    Free Gaza

--- homepage/counter.js
+++ homepage/counter.js
@@ -1,7 +1,38 @@
-const SUPABASE_URL = "IL_TUO_PROJECT_URL";
-const SUPABASE_KEY = "LA_TUA_PUBLISHABLE_KEY";
+const SUPABASE_URL = "https://yzqforpyicopxcadhbhm.supabase.co";
+const SUPABASE_KEY = "sb_publishable_LZq3YpQMzpoVIhmkKK4Ckw_t-6PtsIa";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
+async function countVisit() {
+
+    const { data, error } = await supabaseClient
+        .from("site_stats")
+        .select("visits")
+        .eq("id", 1)
+        .single();
+
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    let newCount = data.visits + 1;
+
+    const { error: updateError } = await supabaseClient
+        .from("site_stats")
+        .update({ visits: newCount })
+        .eq("id", 1);
+
+    if (updateError) {
+        console.log(updateError);
+        return;
+    }
+
+    document.getElementById("signal-counter").innerHTML =
+        `Thank you, observer. This signal is happy to have traveled ${newCount} times across the network.`;
+}
+
+countVisit();
